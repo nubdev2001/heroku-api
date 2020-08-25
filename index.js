dff366436c5006624dd3fbba6b98b68a8b977b03
nubdev2001 <nubdev.2001@gmail.com>
2020-08-25 13:53:54 +0700

    edit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+const socketio = require('socket.io')
 
 app.get('/api',(req,res) => {
     res.json({
@@ -18,6 +19,20 @@ app.post('/api',(req,res) => {
     console.log("New a POST request")
 })
 
-app.listen(port,() => {
+const server = app.listen(port,() => {
     console.log("[Server] => Running Now On PORT : ",port)
+})
+
+const io = socketio.listen(server);
+io.on('connection', client => {
+    console.log('user connected')
+    client.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+app.get('/realtime',(req,res)=>{
+    const value = req.query.value;
+    io.sockets.emit('value', value)
+    res.json({params:value})
 })
